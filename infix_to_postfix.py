df7429cde6b63277bf6bd5fe7613d5271e81f2d0
Joseph Pickert <pickertjoe@gmail.com>
2019-06-19 09:21:28 -0600

    Adding input control and functionality to postfix programs

--- infix_to_postfix.py
+++ infix_to_postfix.py
@@ -1,4 +1,4 @@
-# infexToPostfix duplicated from Miller and Ranum's text
+# infixToPostfix duplicated from Miller and Ranum's text
 # My contribution was to add a feature that accounts for user input error
 # Also created a driver function to make the program usable
 
@@ -9,12 +9,12 @@ import string
 def main():
     """Serves as the main menu and driver function"""
     while True:
-        print("~~~Welcome to the Python Infix to Postfix Converter~~~")
+        print("\n~~~Welcome to the Python Infix to Postfix Converter~~~")
         choice = input('Would you like to proceed?(Y/N): ')
         if choice.title() == 'Y':
-            expression = grab_input()
+            expression, original = grab_input()
             converted = infixToPostfix(expression)
-            show_results(expression, converted)
+            show_results(original, converted)
             continue
 
         elif choice.title() == 'N':
@@ -25,7 +25,7 @@ def main():
             print("Invalid input. Returning to main...")
 
 
-def infixToPostfix(infexexpr):
+def infixToPostfix(tokenList):
     """Converts an infix arithmetic expression to postfix"""
 
     # Creating a dictionary to hold oprator precedence
@@ -38,8 +38,6 @@ def infixToPostfix(infexexpr):
 
     opStack = Stack()
     postfixList = []
-
-    tokenList = infexexpr.split()
 
     for token in tokenList:
         # Adding any operands to the postFix list
@@ -67,4 +65,51 @@ def infixToPostfix(infexexpr):
 def grab_input():
     """A function to collect and screen user input to prevent errors"""
     while True:
-        infexexpr = input("Please enter an appropriate infix arithmetic expression: ")
+        prompt = ("\nPlease enter an appropriate infix arithmetic expression.")
+        prompt += ("\nEnsure to separate each character by a space: ")
+        infexexpr = input(prompt)
+
+        # Splitting user string into a list
+        tokenList = infexexpr.split()
+
+        # Creating incrementors to count number of parentheses, operators, and operands
+        inc1, inc2, inc3, inc4 = (0, 0, 0, 0)
+        operator_list = ['+', '-', '*', '/']
+
+        # Looping through input list to record numbers of each component
+        for token in tokenList:
+            if token == '(':
+                inc1 += 1
+            elif token == ')':
+                inc2 += 1
+            elif token in operator_list:
+                inc3 += 1
+            elif token in string.ascii_uppercase:
+                inc4 += 1
+            elif token == " ":
+                continue
+            else:
+                print("The character " + token + " is invalid.")
+                break
+
+        if inc1 == inc2 and (inc4 - inc3) == 1:
+            return tokenList, infexexpr
+        else:
+            print("\nError detected in input string.")
+            print("Please ensure that all parentheses are closed")
+            print("and that the number of operators is one less than the number of operands.")
+
+            choice = input("Try again?(Y/N): ")
+            if choice.title() == 'Y':
+                continue
+            else:
+                return
+
+
+def show_results(expression, converted):
+    """A function to print the original infix expression and the converted postfix expression"""
+    print("\nThe original infix expression was :" + str(expression))
+    print("The converted postfix expression is: " + str(converted))
+
+
+main()
--- postfix_eval.py
+++ postfix_eval.py
@@ -1,13 +1,32 @@
-# Code ducplicated rom Miller and Ranum's text
+# postfixEval duplicated rom Miller and Ranum's text
+# My contribution was to add a feature that accounts for user input error
+# Also created a driver function to make the program usable
 
 from pythonds.basic import Stack
 
 
-def postfixEval(postfixExpr):
+def main():
+    """Serves as the main menu and driver function"""
+    while True:
+        print("\n~~~Welcome to the Python Postfix Evaluation Calculator~~~")
+        choice = input('Would you like to proceed?(Y/N): ')
+        if choice.title() == 'Y':
+            expression, original = grab_input()
+            converted = postfixEval(expression)
+            show_results(original, converted)
+            continue
+
+        elif choice.title() == 'N':
+            print("Closing program...")
+            break
+
+        else:
+            print("Invalid input. Returning to main...")
+
+
+def postfixEval(tokenList):
     """Arithmentically evaluates the result of a postfix expression"""
     operandStack = Stack()
-
-    tokenList = postfixExpr.split()
 
     for token in tokenList:
         if token in '0123456789':
@@ -31,3 +50,54 @@ def doMath(op, op1, op2):
         return op1 + op2
     else:
         return op1 - op2
+
+
+def grab_input():
+    """A function to collect and screen user input to prevent errors"""
+    while True:
+        prompt = ("\nPlease enter an appropriate postfix arithmetic expression.")
+        prompt += ("\nEnsure to separate each character by a space")
+        prompt += ("\nAnd use only numbers and arithmetic operators: ")
+        postfixexpr = input(prompt)
+
+        # Splitting user string into a list
+        tokenList = postfixexpr.split()
+        # Creating incrementors to count number of parentheses, operators, and operands
+        inc1, inc2 = (0, 0)
+        operator_list = ['+', '-', '*', '/']
+        # A flag to activate if an invalid character is encountered in the input string
+        flag = False
+
+        # Looping through input list to record numbers of each component
+        for token in tokenList:
+            if token in operator_list:
+                inc1 += 1
+            elif token in '0123456789':
+                inc2 += 1
+            elif token == " ":
+                continue
+            else:
+                print("The character " + token + " is invalid.")
+                flag = True
+
+        if (inc2 - inc1) == 1 and flag is True:
+            return tokenList, postfixexpr
+        else:
+            print("\nError detected in input string.")
+            print("Please ensure that all parentheses are closed")
+            print("and that the number of operators is one less than the number of operands.")
+
+            choice = input("Try again?(Y/N): ")
+            if choice.title() == 'Y':
+                continue
+            else:
+                return
+
+
+def show_results(expression, converted):
+    """A function to print the original infix expression and the converted postfix expression"""
+    print("\nThe original postfix expression was :" + str(expression))
+    print("The calculated value of the expression is: " + str(converted))
+
+
+main()
