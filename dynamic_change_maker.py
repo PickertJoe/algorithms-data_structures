27d17c948f37f3c81aaed79b9da2814ff2f29e59
Joseph Pickert <pickertjoe@gmail.com>
2019-07-16 14:55:13 -0600

    Dynamic programming example from text

---
+++ dynamic_change_maker.py
@@ -0,0 +1,23 @@
+# A dynamic programming solution to solve for the fewest number of coins needed to make change
+# Code sourced from Miller and Ranum's Problem Solving with Algorithms & Data Structures Using Python
+
+
+def dpMakeChange(coinValueList, change, minCoins, coinsUsed):
+    for cents in range(change + 1):
+        coinCount = cents
+        newCoin = 1
+        for j in [c for c in coinValueList if c <= cents]:
+            if minCoins[cents - j] + 1 < coinCount:
+                coinCount = minCoins[cents - j] + 1
+                newCoin = j
+        minCoins[cents] = coinCount
+        coinsUsed[cents] = newCoin
+    return minCoins[change]
+
+
+def printCoins(coinsUsed, change):
+    coin = change
+    while coin > 0:
+        thisCoin = coinsUsed[coin]
+        print(thisCoin)
+        coin = coin - thisCoin
--- maze_explorer.py
+++ maze_explorer.py
@@ -35,6 +35,67 @@ class Maze:
                             (columnsinMaze - 1) / 2 + .5,
                             (rowsinMaze - 1) / 2 + .5)
 
+    def drawMaze(self):
+        for y in range(self.rowsinMaze):
+            for x in range(self.columnsinMaze):
+                if self.mazelist[y][x] == OBSTACLE:
+                    self.drawCenteredBox(x + self.xTranslate,
+                                         -y + self.yTranslate,
+                                         'tan')
+        self.t.color('black', 'blue')
+
+    def drawCenteredBox(self, x, y, color):
+        tracer(0)
+        self.t.up()
+        self.t.goto(x - .5, y - .5)
+        self.t.color('black', color)
+        self.t.setheading(90)
+        self.t.down()
+        self.t.being_fill()
+        for i in range(4):
+            self.t.forward(1)
+            self.t.right(90)
+        self.t.end_fill()
+        update()
+        tracer(1)
+
+    def moveTurtle(self, x, y):
+        self.t.up()
+        self.t.setheading(self.t.towards(x + self.xTranslate,
+                                         -y + self.yTranslate))
+        self.t.goto(x + self.xTranslate, -y + self.yTranslate)
+
+    def dropBreadcrumb(self, color):
+        self.t.dot(color)
+
+    def updatePosition(self, row, col, val=None):
+        if val:
+            self.mazelist[row][col] = val
+        self.moveTurtle(col, row)
+
+        if val == PART_OF_PATH:
+            color = 'green'
+        elif val == OBSTACLE:
+            color = 'red'
+        elif val == TRIED:
+            color = 'black'
+        elif val == DEAD_END:
+            color = 'red'
+        else:
+            color = None
+
+        if color:
+            self.dropBreadcrumb(color)
+
+    def isExit(self, row, col):
+        return (row == 0 or
+                row == self.rowsinMaze - 1 or
+                col == 0 or
+                col == self.columnsinMaze - 1)
+
+    def __getitem__(self, idx):
+        return self.mazelist[idx]
+
 
 def searchFrom(maze, startRow, startCol):
     maze.updatePosition(startRow, startCol)
