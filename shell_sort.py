124d34bf8cb3fde13a3947b598c3a5a8007e2ac1
dilbert250 <pickertjoe@gmail.com>
2019-08-19 15:57:01 -0500

    Implementing shell sort

---
+++ shell_sort.py
@@ -0,0 +1,25 @@
+# A function to sort a numeric list via shell and insertion sorting. Sourced from Miller & Ranum
+
+
+def shellSort(alist):
+    sublistcount = len(alist) // 2
+    while sublistcount > 0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
+
+        print("After increments of size ", sublistcount, "This is ", alist)
+
+        sublistcount //= 2
+
+
+def gapInsertionSort(alist, start, gap):
+    for i in range(start + gap, len(alist), gap):
+
+        currentvalue = alist[i]
+        position = i
+
+        while position >= gap and alist[position - gap] > currentvalue:
+            alist[position] = alist[position - gap]
+            position = position - gap
+
+        alist[position] = currentvalue
