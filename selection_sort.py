83c5f3c2829e34d15b9b665f5e4cf1a0949f83d1
dilbert250 <pickertjoe@gmail.com>
2019-08-19 15:51:19 -0500

    Implementing selection sort

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+# A simple function to perform a selection sort to order a numeric list. Sourced from Miller & Ranum
+
+
+def selectionSort(alist):
+    for fillslot in range(len(alist) - 1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, fillslot + 1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
+
+        temp = alist[fillslot]
+        alist[fillslot] = alist[positionOfMax]
+        alist[positionOfMax] = temp
