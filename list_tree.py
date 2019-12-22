b24f19bd54c20789636a2d6da220d76b9d5ca629
dilbert250 <pickertjoe@gmail.com>
2019-12-22 16:42:50 -0600

    Adding basic edit and access functionality to list tree

--- list_tree.py
+++ list_tree.py
@@ -21,3 +21,19 @@ def insertRight(root, newBranch):
     else:
         root.insert(2, [newBranch], t, [])
     return root
+
+
+def getRootVal(root):
+    return root[0]
+
+
+def setRootVal(root, newVal):
+    root[0] = newVal
+
+
+def getLeftChild(root):
+    return root[1]
+
+
+def getRightChild(root):
+    return root[2]
