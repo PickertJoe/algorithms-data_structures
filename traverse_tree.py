1176e35a6e96545dbdde6323590be85c0e4e33c1
dilbert250 <pickertjoe@gmail.com>
2020-01-04 16:27:46 -0600

    Adding inorder traverse method; print method

--- traverse_tree.py
+++ traverse_tree.py
@@ -13,3 +13,19 @@ def postorder(tree):
         postorder(tree.getLeftChild())
         postorder(tree.getRightChild())
         print(tree.getRootVal())
+
+
+def inorder(tree):
+    if tree is not None:
+        inorder(tree.getLeftChild())
+        print(tree.getRootVal())
+        inorder(tree.getRightChild())
+
+
+def printexp(tree):
+    sVal = ""
+    if tree:
+        sVal = '(' + printexp(tree.getLeftChild())
+        sVal = sVal + str(tree.getRootVal())
+        sVal = sVal + printexp(tree.getRightChild()) + ')'
+    return sVal
