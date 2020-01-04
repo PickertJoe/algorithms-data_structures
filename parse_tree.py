5d3f7f81fdd3292175c6e69f8aa3a0f30d582fc1
dilbert250 <pickertjoe@gmail.com>
2020-01-04 15:57:49 -0600

    Adding recursive function to evaluate a parse tree

--- parse_tree.py
+++ parse_tree.py
@@ -28,3 +28,16 @@ def buildParseTree(fpexp):
         else:
             raise ValueError("Unknown Operator: " + i)
     return eTree
+
+
+def evaluate(parseTree):
+    opers = {'+': operator.add, '-': operator: sub,
+             '*': operator.mul, '/': operator: truediv}
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
